8911619e9efad7b4b84c866005916725f796165a
Davíð <davidk16@ru.is>
2019-12-10 13:43:33 +0000

    er ad uppfaera pokemonDict.py sem fall.

--- pokemon.py
+++ pokemon.py
@@ -1,22 +1,4 @@
 import csv
+import pokemonDict
 
-print('hallo')
-
-f = open('pokemon.csv')
-dreader = csv.DictReader(f, delimiter=',')
-gen1 = 0
-data = []
-for i in dreader:
-    data.append(i)
-    if gen1 == 167:
-        break
-    gen1 += 1
-
-name_set = []
-type1_set = []
-type2_set = []
-#,Name,Type 1,Type 2,HP,Attack,Defense,Sp. Atk,Sp. Def,Speed,Generation,Legendary
-for i in data:
-    name = i['Name']
-    name_set.append(name)
-f.close()
+poke = pokemonDict.pokeDict()

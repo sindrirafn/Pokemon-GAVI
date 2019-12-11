db61d6031b0f2287cba2494606a70d279639900a
Jón Evert Pálsson <jonevertpalsson@Jon-Everts-MacBook-Pro-4.local>
2019-12-11 14:51:55 +0000

    var ad laga movesReader.py stippa burt '-'

--- movesReader.py
+++ movesReader.py
@@ -11,24 +11,25 @@ def import_moves():
     moveDict = {}
     for row in data:
         if int(row['Generation']) == 1:
-            moveDict[row['Name']] = {'ID': 0, 'type': '', 'category': '', 'pp': 0, 'power':0,'acc': 0,}
+            moveDict[row['Name'].replace('-','')] = {'ID': 0, 'type': '', 'category': '', 'pp': 0, 'power':0,'acc': 0,}
 
     cnt = 0                                                     # indexa oll moves og byrja i 0
 
     for row in data:
         if int(row['Generation']) == 1:                         # tek bara generation 1
-            moveDict[row['Name']]['ID'] = cnt
-            moveDict[row['Name']]['type'] = row['Type']
-            moveDict[row['Name']]['category'] = row['Category']
-            moveDict[row['Name']]['pp'] = int(row['PP'])
+            print(row['Name'].replace('-',''))
+            moveDict[row['Name'].replace('-','')]['ID'] = cnt
+            moveDict[row['Name'].replace('-','')]['type'] = row['Type']
+            moveDict[row['Name'].replace('-','')]['category'] = row['Category']
+            moveDict[row['Name'].replace('-','')]['pp'] = int(row['PP'])
             if row['Power'] == 'None':
-                moveDict[row['Name']]['power'] = 100
+                moveDict[row['Name'].replace('-','')]['power'] = 100
             else:
-                moveDict[row['Name']]['power'] = int(row['Power'])
+                moveDict[row['Name'].replace('-','')]['power'] = int(row['Power'])
             if row['Accuracy'] == 'None':
-                moveDict[row['Name']]['acc'] = 100
+                moveDict[row['Name'].replace('-','')]['acc'] = 100
             else:
-                moveDict[row['Name']]['acc'] = int(row['Accuracy'])
+                moveDict[row['Name'].replace('-','')]['acc'] = int(row['Accuracy'])
             cnt += 1
-    return moveDict
+    return(moveDict)
 
